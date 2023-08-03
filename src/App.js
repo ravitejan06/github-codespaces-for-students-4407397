07f387999a12ae2f71634628ca08d5c7d690cc04
ravitejan06 <87709422+ravitejan06@users.noreply.github.com>
2023-08-03 12:10:40 +0000

    Chnaged the header

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Hey People <span className="heart">♥️</span>
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
+          I am taking Codespaces course.
         </p>
         <p>
           <a
